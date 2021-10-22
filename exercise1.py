7cf3de4fae363fd05cd0f7bb8a3e1d086d1f242f
Aibek2002 <Aibekabeken47@gmail.com>
2021-10-22 21:03:16 +0500

    seminar3

--- exercise1.py
+++ exercise1.py
@@ -1,3 +1,10 @@
+try:
+	a =int(input("Number1:"))
+	b =int(input("Number2:"))
+	c =int(input("Number3:"))
+except:
+	print("Error")
+	exit()
 def max_number(num1,num2,num3):
 	if num1 > num2 and num1 > num3 :
 		print("maximum number:",num1)
@@ -5,4 +12,4 @@ def max_number(num1,num2,num3):
 		print("maximum number:",num2)
 	else :
 		print("maximum number:",num3)
-max_number(1,2,3)
+max_number(a,b,c)
